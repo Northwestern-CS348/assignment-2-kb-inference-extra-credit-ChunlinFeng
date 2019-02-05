dd4096dceeae4374814b0fe1eaba63260769985d
feng <Chunlinfeng2020@u.northwestern.edu>
2019-02-05 15:14:20 -0600

    Answer beta 2 with syn

--- student_code.py
+++ student_code.py
@@ -9,6 +9,7 @@ class KnowledgeBase(object):
         self.facts = facts
         self.rules = rules
         self.ie = InferenceEngine()
+        self.space_count = 0
 
     def __repr__(self):
         return 'KnowledgeBase({!r}, {!r})'.format(self.facts, self.rules)
@@ -127,8 +128,107 @@ class KnowledgeBase(object):
         """
         printv("Retracting {!r}", 0, verbose, [fact_or_rule])
         ####################################################
-        # Implementation goes here
-        # Not required for the extra credit assignment
+        # Student code goes here
+
+        # List several rules here
+        # An asserted fact should only be removed if it is unsupported.
+        # Rules must never be retracted and an asserted rule should never be removed.
+        # Use the supports_rules and supports_facts fields to find and adjust facts and rules that are supported by a retracted fact.
+        # The supported_by lists in each fact/rule that it supports needs to be adjusted accordingly.
+        # If a supported fact/rule is no longer supported as a result of retracting this fact (and is not asserted), it should also be removed.
+
+        # if fact or rule has more than one supported_by, do not remove
+        if isinstance(fact_or_rule, Fact):
+            if fact_or_rule not in self.facts:
+                return None
+            else:
+                ind = self.facts.index(fact_or_rule)
+                fact_or_rule = self.facts[ind]
+        elif isinstance(fact_or_rule, Rule):
+            if fact_or_rule not in self.rules:
+                return None
+            else:
+                ind = self.rules.index(fact_or_rule)
+                fact_or_rule = self.rules[ind]
+
+
+
+        if len(fact_or_rule.supported_by)>0: pass;
+        else:
+        # able to delete
+            if isinstance(fact_or_rule, Fact):
+                # find the fact
+
+                for item in fact_or_rule.supports_facts:
+                    # syn the item
+                    if item in self.facts:
+                        index_item = self.facts.index(item)
+                        item = self.facts[index_item]
+
+                    # find the supported one and del it
+                    i_flag = - 0;
+                    length = len(item.supported_by)
+                    for i in range(length):
+                        real_i = i + i_flag
+                        if item.supported_by[real_i][0] == fact_or_rule:
+                            del item.supported_by[real_i]
+                            i_flag -= 1
+                            self.kb_retract(item)
+                for item in fact_or_rule.supports_rules:
+                    # syn the item
+                    if item in self.rules:
+                        index_item = self.rules.index(item)
+                        item = self.rules[index_item]
+
+                    i_flag = - 0;
+                    length = len(item.supported_by)
+                    for i in range(length):
+                        real_i = i + i_flag
+                        if item.supported_by[real_i][0] == fact_or_rule:
+                            del item.supported_by[real_i]
+                            i_flag -= 1
+                            self.kb_retract(item)
+
+                # del the fact_or_rule
+                ind = self.facts.index(fact_or_rule)
+                del self.facts[ind]
+
+            elif isinstance(fact_or_rule, Rule):
+                if fact_or_rule.asserted == True: pass;
+                else:
+                    for item in fact_or_rule.supports_facts:
+                        # syn the item
+                        if item in self.facts:
+                            index_item = self.facts.index(item)
+                            item = self.facts[index_item]
+
+                        # find the supported one and del it
+                        i_flag = - 0;
+                        length = len(item.supported_by)
+                        for i in range(length):
+                            real_i = i + i_flag
+                            if item.supported_by[real_i][1] == fact_or_rule:
+                                del item.supported_by[real_i]
+                                i_flag -= 1
+                                self.kb_retract(item)
+                    for item in fact_or_rule.supports_rules:
+                        # syn the item
+                        if item in self.rules:
+                            index_item = self.rules.index(item)
+                            item = self.rules[index_item]
+
+                        i_flag = - 0;
+                        length = len(item.supported_by)
+                        for i in range(length):
+                            real_i = i + i_flag
+                            if item.supported_by[real_i][1] == fact_or_rule:
+                                del item.supported_by[real_i]
+                                i_flag -= 1
+                                self.kb_retract(item)
+
+                    # del the fact_or_rule
+                    ind = self.rules.index(fact_or_rule)
+                    del self.rules[ind]
 
     def kb_explain(self, fact_or_rule):
         """
@@ -142,6 +242,84 @@ class KnowledgeBase(object):
         """
         ####################################################
         # Student code goes here
+
+        # sample is a FACT
+        if isinstance(fact_or_rule, Fact):
+            # not exist in KB
+            if fact_or_rule not in self.facts: return "Fact is not in the KB"
+            else:
+                # get the right fact and convert
+                fact = self._get_fact(fact_or_rule)
+                result = self.statement_converter(fact,1,self.space_count)
+
+                if len(fact.supported_by) >0:
+                    for supported_by in fact.supported_by:
+                        result += "\n" + self.statement_converter("", 0, self.space_count)  # +SUPPORTED BY
+                        statement = "\n    " + self.statement_converter(supported_by[0], 1,self.space_count) + "\n"
+                        statement += "    " + self.statement_converter(supported_by[1], 2,self.space_count)
+                        result += statement
+
+                        inner_rule = self._get_rule(supported_by[1])
+                        if len(inner_rule.supported_by) > 0:
+                            self.space_count += 1;
+                            result += self.kb_explain(inner_rule)
+                            self.space_count -= 1;
+
+                return result
+
+
+
+
+        # sample is a RULE
+        elif isinstance(fact_or_rule, Rule):
+            # Rule not exist in KB
+            if fact_or_rule not in self.rules: return "Rule is not in the KB"
+            else:
+                # get the right rule and convert
+                rule = self._get_rule(fact_or_rule)
+                statement = self.statement_converter(rule,2,self.space_count)
+
+                if len(rule.supported_by) >0:
+                    for supported_by in rule.supported_by:
+                        result = "\n" + self.statement_converter("", 0, self.space_count)  # +SUPPORTED BY
+                        statement = "\n    " + self.statement_converter(supported_by[0], 1, self.space_count) + "\n"
+                        statement += "    " + self.statement_converter(supported_by[1], 2, self.space_count)
+                        result += statement
+
+                        inner_rule = self._get_rule(supported_by[1])
+                        if len(inner_rule.supported_by) > 0:
+                            self.space_count += 1;
+                            result += self.kb_explain(inner_rule)
+                            self.space_count -= 1;
+                return result
+
+
+
+
+
+
+        else: return False;
+
+    def statement_converter(self, fact_or_rule, type, space_count):
+        if type == 0: return "    "*space_count + "  " + "SUPPORTED BY";
+        if type == 1:
+            result = "fact: " + str(fact_or_rule.statement)
+
+        elif type == 2:
+            lhs_length = len(fact_or_rule.lhs)
+            if lhs_length == 0:
+                lhs = str(fact_or_rule.lhs);
+            else:
+                lhs = str(fact_or_rule.lhs[0])
+                for ind,item in enumerate(fact_or_rule.lhs[1:]):
+                    lhs += ", " + str(item)
+
+            rhs = str(fact_or_rule.rhs)
+            result ="rule: " + "(" + lhs + ")" + " -> " + rhs
+
+        if fact_or_rule.asserted == True: result += " ASSERTED";
+        return "    "*space_count + result
+
 
 
 class InferenceEngine(object):
@@ -161,3 +339,51 @@ class InferenceEngine(object):
         ####################################################
         # Implementation goes here
         # Not required for the extra credit assignment
+        # match LHS and fact
+        match_result = match(fact.statement, rule.lhs[0])
+
+        # successfully match a LHS and a Rule
+        # which means a new rule or fact can be generate
+        if match_result != False:
+
+            # if LHS only have one statement, then it is a new fact
+            if len(rule.lhs) == 1:
+                # in this condition, no new rule will generate
+                fact_add = Fact(instantiate(rule.rhs, match_result))
+
+                # modify the support area
+                fact_add.asserted = False
+                # add supported_by rule and facts
+                fact_add.supported_by.append([fact, rule])
+
+                # merge supports_fact and rules
+                fact.supports_facts.append(fact_add)
+                rule.supports_facts.append(fact_add)
+
+                # add the new fact and modified rule
+                kb.kb_add(fact_add)
+
+            elif len(rule.lhs) > 1:
+                # a new rule can be generated
+                rule_add = copy.deepcopy(rule)
+                rule_add.lhs = []
+                rule_add.rhs = []
+
+                # LHS
+                for statements in rule.lhs[1:]:
+                    new_statement = instantiate(statements, match_result)
+                    rule_add.lhs.append(new_statement)
+                # RHS
+                rule_add.rhs = instantiate(rule.rhs, match_result)
+
+                rule_add.asserted = False
+                # modify the support area
+                # merge supported_by
+                rule_add.supported_by.append([fact, rule])
+
+                # merge supports_rule
+                fact.supports_rules.append(rule_add)
+                rule.supports_rules.append(rule_add)
+
+                # add the new rule and modified fact
+                kb.kb_add(rule_add)
